e8169c9be7ea9bb6b15dce7f7bcd3336c26a78fa
Eric Solari <beachandbytes@gmail.com>
2014-09-17 12:28:37 -1000

    Finished Test Project and added rest of MVC config for jasmine test runner page (Useful for debugging)

--- FoundationAngular.Test/Controllers/JasmineController.cs
+++ FoundationAngular.Test/Controllers/JasmineController.cs
@@ -2,8 +2,10 @@ using System.Web.Mvc;
 
 namespace FoundationAngular.Test.Controllers
 {
+    [RoutePrefix("")]
     public class JasmineController : Controller
     {
+        [Route]
         public ViewResult Run()
         {
             return View("SpecRunner");
